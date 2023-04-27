c3f069c2acf575879300428c62f10d5c7940edb1
KDuk0 <125793699+KDuk0@users.noreply.github.com>
2023-04-27 11:51:21 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,25 @@ use tokio::time::{delay_for, Duration};
 
 #[tokio::main]
 async fn main() {
-    let async_task = spawn(async {
-        println!("Async task started.");
-        delay_for(Duration::from_secs(1)).await;
-        println!("Async task done.");
-    });
+    let mut tasks = vec![];
+    for id in 0..5 {
+        let t = spawn(async move {
+            println!("Async task {} started.", id);
+            delay_for (Duration::from_millis((5-id) * 100)).await;
+            println!("Async task {} done.", id);
+            let result = id * id;
+            (id, result)
+        });
 
-    println!("Launching task...");
-    async_task.await.expect("asyinc task failed");
-    println!("ready!");
+        tasks.push(t);
+    }
+    
+    println!("Launched {} tasks...", tasks.len());
+    for task in tasks {
+        let (id, result) = task.await.expect("task failed");
+        println!("Task {} completed with result: {}", id, result);
+    }
+
+    println!("Ready");
+    
 }
